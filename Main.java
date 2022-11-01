87fa0ed5c647807e0a9196434829acbeae254336
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-01 15:52:44 +0000

    Math random

--- Main.java
+++ Main.java
@@ -6,5 +6,21 @@ class Main {
     //One additional number between 1-26
     //
     //Powerball: 1,200,000,000 - match all 5 and powerball
+
+    //$1,000,000 - match 5
+    //$50,000- match 4+powerball
+    //$100 -match 4 +powerball
+    //$7-match 3
+    //$7-match 2 +powerball
+    //$4-match 1 + powerball, 0 + powerball
+
+    public static int[] makeTicket(){
+      int[] ticket = {0,0,0,0,0,0};
+      int counter = 0;
+      while(counter<6){
+        int numToTry = (int)(Math.random()* 69 +1);
+      }
+    }
+    
   }
 }
