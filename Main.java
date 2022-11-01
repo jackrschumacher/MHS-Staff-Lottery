afd30f7797bd3e52a25f3e76dacc74c81e1b2f2a
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-01 15:42:39 +0000

    Add Comments

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    //Powerball costs $2 per ticket
+    //
+    //5 numbers between 1-69
+    //One additional number between 1-26
+    //
+    //Powerball: 1,200,000,000 - match all 5 and powerball
   }
 }
